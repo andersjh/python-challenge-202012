27829339564509a6810d207408da28e800f24d1b
andersjh <andersjh120@gmail.com>
2021-01-30 09:45:07 -0600

    proved csv reader

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+import csv
+
+file_name = "Resources/election_data.csv"
+
+with open(file_name) as fh:
+    csv_reader = csv.reader(fh)
+
+    column_headers = next(csv_reader)
+
+    for row in csv_reader:
+        print(row)
+        break
